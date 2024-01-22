bb9ea5f89eb6980c5456e029797e786a5956d7f2
AS627 <46211021+AS627@users.noreply.github.com>
2024-01-22 16:05:53 -0600

    Add files via upload

---
+++ controller_ae483_custom_k.c
@@ -0,0 +1,347 @@
+#include "controller_ae483.h"
+#include "stabilizer_types.h"
+#include "power_distribution.h"
+#include "log.h"
+#include "param.h"
+#include "num.h"
+#include "math3d.h"
+
+// Sensor measurements
+// - tof (from the z ranger on the flow deck)
+static uint16_t tof_count = 0;
+static float tof_distance = 0.0f;
+// - flow (from the optical flow sensor on the flow deck)
+static uint16_t flow_count = 0;
+static float flow_dpixelx = 0.0f;
+static float flow_dpixely = 0.0f;
+
+// Parameters
+static bool use_observer = false;
+static bool reset_observer = false;
+
+// State
+static float o_x = 0.0f;
+static float o_y = 0.0f;
+static float o_z = 0.0f;
+static float psi = 0.0f;
+static float theta = 0.0f;
+static float phi = 0.0f;
+static float v_x = 0.0f;
+static float v_y = 0.0f;
+static float v_z = 0.0f;
+static float w_x = 0.0f;
+static float w_y = 0.0f;
+static float w_z = 0.0f;
+
+// Setpoint
+static float o_x_des = 0.0f;
+static float o_y_des = 0.0f;
+static float o_z_des = 0.0f;
+
+// Input
+static float tau_x = 0.0f;
+static float tau_y = 0.0f;
+static float tau_z = 0.0f;
+static float f_z = 0.0f;
+
+// Motor power command
+static uint16_t m_1 = 0;
+static uint16_t m_2 = 0;
+static uint16_t m_3 = 0;
+static uint16_t m_4 = 0;
+
+// Measurements
+static float n_x = 0.0f;
+static float n_y = 0.0f;
+static float r = 0.0f;
+static float a_z = 0.0f;
+
+// Constants
+static float k_flow = 4.09255568f;
+static float g = 9.81f;
+static float dt = 0.002f;
+static float o_z_eq = 0.5f;
+
+// Measurement errors
+static float n_x_err = 0.0f;
+static float n_y_err = 0.0f;
+static float r_err = 0.0f;
+
+/////////////////////////////////////// KALMAN FILTER VARIABLES
+// O_Z
+float oz_measured;
+float oz_estimated;
+float K_oz;
+float P_temp_oz;
+float P_last_oz;
+float oz_est_new;
+static float Q_oz = 0.1f;
+static float R_oz = 0.06350709061837238f*0.06350709061837238f;
+
+// V_X
+float vx_measured;
+float vx_estimated;
+float K_vx;
+float P_temp_vx;
+float P_last_vx;
+float vx_est_new;
+static float Q_vx = 0.1f;
+static float R_vx = 0.7570370678653842f*0.7570370678653842f;
+
+// V_Y
+float vy_measured;
+float vy_estimated;
+float K_vy;
+float P_temp_vy;
+float P_last_vy;
+float vy_est_new;
+static float Q_vy = 0.1f;
+static float R_vy = 0.7698969611565406f*0.7698969611565406f;
+
+void ae483UpdateWithTOF(tofMeasurement_t *tof)
+{
+  tof_distance = tof->distance;
+  tof_count++;
+}
+
+void ae483UpdateWithFlow(flowMeasurement_t *flow)
+{
+  flow_dpixelx = flow->dpixelx;
+  flow_dpixely = flow->dpixely;
+  flow_count++;
+}
+
+void ae483UpdateWithDistance(distanceMeasurement_t *meas)
+{
+  // If you have a loco positioning deck, this function will be called
+  // each time a distance measurement is available. You will have to write
+  // code to handle these measurements. These data are available:
+  //
+  //  meas->anchorId  uint8_t   id of anchor with respect to which distance was measured
+  //  meas->x         float     x position of this anchor
+  //  meas->y         float     y position of this anchor
+  //  meas->z         float     z position of this anchor
+  //  meas->distance  float     the measured distance
+}
+
+void ae483UpdateWithPosition(positionMeasurement_t *meas)
+{
+  // This function will be called each time you send an external position
+  // measurement (x, y, z) from the client, e.g., from a motion capture system.
+  // You will have to write code to handle these measurements. These data are
+  // available:
+  //
+  //  meas->x         float     x component of external position measurement
+  //  meas->y         float     y component of external position measurement
+  //  meas->z         float     z component of external position measurement
+}
+
+void ae483UpdateWithPose(poseMeasurement_t *meas)
+{
+  // This function will be called each time you send an external "pose" measurement
+  // (position as x, y, z and orientation as quaternion) from the client, e.g., from
+  // a motion capture system. You will have to write code to handle these measurements.
+  // These data are available:
+  //
+  //  meas->x         float     x component of external position measurement
+  //  meas->y         float     y component of external position measurement
+  //  meas->z         float     z component of external position measurement
+  //  meas->quat.x    float     x component of quaternion from external orientation measurement
+  //  meas->quat.y    float     y component of quaternion from external orientation measurement
+  //  meas->quat.z    float     z component of quaternion from external orientation measurement
+  //  meas->quat.w    float     w component of quaternion from external orientation measurement
+}
+
+void ae483UpdateWithData(const struct AE483Data* data)
+{
+  // This function will be called each time AE483-specific data are sent
+  // from the client to the drone. You will have to write code to handle
+  // these data. For the example AE483Data struct, these data are:
+  //
+  //  data->x         float
+  //  data->y         float
+  //  data->z         float
+  //
+  // Exactly what "x", "y", and "z" mean in this context is up to you.
+}
+
+
+void controllerAE483Init(void)
+{
+  // Do nothing
+}
+
+bool controllerAE483Test(void)
+{
+  // Do nothing (test is always passed)
+  return true;
+}
+
+void controllerAE483(control_t *control,
+                     setpoint_t *setpoint,
+                     const sensorData_t *sensors,
+                     const state_t *state,
+                     const uint32_t tick)
+{
+  if (RATE_DO_EXECUTE(ATTITUDE_RATE, tick)) {
+    // Everything in here runs at 500 Hz
+
+    // Desired position
+    o_x_des = setpoint->position.x;
+    o_y_des = setpoint->position.y;
+    o_z_des = setpoint->position.z;
+
+    // Measurements
+    w_x = radians(sensors->gyro.x);
+    w_y = radians(sensors->gyro.y);
+    w_z = radians(sensors->gyro.z);
+    a_z = g * sensors->acc.z;
+    n_x = flow_dpixelx;
+    n_y = flow_dpixely;
+    r = tof_distance;
+
+    if (reset_observer) {
+      o_x = 0.0f;
+      o_y = 0.0f;
+      o_z = 0.0f;
+      psi = 0.0f;
+      theta = 0.0f;
+      phi = 0.0f;
+      v_x = 0.0f;
+      v_y = 0.0f;
+      v_z = 0.0f;
+      reset_observer = false;
+    }
+
+    // State estimates
+    if (use_observer) {
+    
+      // Compute each element of:
+      //   C x + D u - y
+      n_x_err = k_flow * ((v_x / o_z_eq) - w_y) - n_x;
+      n_y_err = k_flow * (w_x + (v_y / o_z_eq)) - n_y;
+      r_err = o_z - r;
+
+      // Update estimates LQR
+      o_x += dt * v_x;
+      o_y += dt * v_y;
+      psi += dt * w_z;
+      theta += dt * (w_y - 0.029925f*n_x_err);
+      phi += dt * (w_x - (-0.024252f*n_y_err));
+      v_z += dt * (a_z - g - 5.676619f*r_err);
+
+      /////////////////////////////////////// SIMPLE KALMAN FILTER
+      // Update estimates for Kalman Filter
+      o_z += dt * v_z;
+      v_x += dt * g*theta;
+      v_y += dt * -g*phi;
+
+      // O_Z
+      for (int i=0;i<10;i++) {
+      oz_measured = r;
+      oz_estimated = o_z;
+
+      P_temp_oz = P_last_oz + Q_oz;
+      K_oz = P_temp_oz * (1.0f/(P_temp_oz + R_oz));
+
+      o_z = oz_estimated + K_oz * (oz_measured - oz_estimated); 
+      P_last_oz = (1.0f-K_oz) * P_temp_oz;
+      }
+
+      // V_X
+      for (int i=0;i<10;i++) {
+      vx_measured = n_x*r/k_flow + r*w_y;
+      vx_estimated = v_x;
+
+      P_temp_vx = P_last_vx + Q_vx;
+      K_vx = P_temp_vx * (1.0f/(P_temp_vx + R_vx));
+
+      v_x = vx_estimated + K_vx * (vx_measured - vx_estimated); 
+      P_last_vx = (1.0f-K_vx) * P_temp_vx;
+      }
+
+      // V_Y
+      for (int i=0;i<10;i++) {
+      vy_measured = n_y*r/k_flow - r*w_x;
+      vy_estimated = v_y;
+
+      P_temp_vy = P_last_vy + Q_vy;
+      K_vy = P_temp_vy * (1.0f/(P_temp_vy + R_vy));
+
+      v_y = vy_estimated + K_vy * (vy_measured - vy_estimated); 
+      P_last_vy = (1.0f-K_vy) * P_temp_vy;
+      }
+      
+    } else {
+
+      o_x = state->position.x;
+      o_y = state->position.y;
+      o_z = state->position.z;
+      psi = radians(state->attitude.yaw);
+      theta = - radians(state->attitude.pitch);
+      phi = radians(state->attitude.roll);
+      v_x = state->velocity.x*cosf(psi)*cosf(theta) + state->velocity.y*sinf(psi)*cosf(theta) - state->velocity.z*sinf(theta);
+      v_y = state->velocity.x*(sinf(phi)*sinf(theta)*cosf(psi) - sinf(psi)*cosf(phi)) + state->velocity.y*(sinf(phi)*sinf(psi)*sinf(theta) + cosf(phi)*cosf(psi)) + state->velocity.z*sinf(phi)*cosf(theta);
+      v_z = state->velocity.x*(sinf(phi)*sinf(psi) + sinf(theta)*cosf(phi)*cosf(psi)) + state->velocity.y*(-sinf(phi)*cosf(psi) + sinf(psi)*sinf(theta)*cosf(phi)) + state->velocity.z*cosf(phi)*cosf(theta);
+    }
+
+    if (setpoint->mode.z == modeDisable) {
+      // If there is no desired position, then all
+      // motor power commands should be zero
+
+      powerSet(0, 0, 0, 0);
+    } else {
+      // Otherwise, motor power commands should be
+      // chosen by the controller
+
+      tau_x = 0.00239430f * (o_y - o_y_des) -0.00346463f * phi + 0.00135445f * v_y -0.00047651f * w_x;
+      tau_y = -0.00223966f * (o_x - o_x_des) -0.00734151f * theta -0.00186963f * v_x -0.00129356f * w_y;
+      tau_z = -0.00164210f * psi -0.00039822f * w_z;
+      f_z = -0.11471886f * (o_z - o_z_des) -0.09147906f * v_z + 0.35217900f;
+
+      m_1 = limitUint16( -3706927.3f * tau_x -3706927.3f * tau_y -38218981.7f * tau_z + 122328.6f * f_z );
+      m_2 = limitUint16( -3706927.3f * tau_x + 3706927.3f * tau_y + 38218981.7f * tau_z + 122328.6f * f_z );
+      m_3 = limitUint16( 3706927.3f * tau_x + 3706927.3f * tau_y -38218981.7f * tau_z + 122328.6f * f_z );
+      m_4 = limitUint16( 3706927.3f * tau_x -3706927.3f * tau_y + 38218981.7f * tau_z + 122328.6f * f_z );
+      
+      // Apply motor power commands
+      powerSet(m_1, m_2, m_3, m_4);
+    }
+  }
+}
+
+LOG_GROUP_START(ae483log)
+LOG_ADD(LOG_UINT16,         num_tof,                &tof_count)
+LOG_ADD(LOG_UINT16,         num_flow,               &flow_count)
+LOG_ADD(LOG_FLOAT,          o_x,                    &o_x)
+LOG_ADD(LOG_FLOAT,          o_y,                    &o_y)
+LOG_ADD(LOG_FLOAT,          o_z,                    &o_z)
+LOG_ADD(LOG_FLOAT,          psi,                    &psi)
+LOG_ADD(LOG_FLOAT,          theta,                  &theta)
+LOG_ADD(LOG_FLOAT,          phi,                    &phi)
+LOG_ADD(LOG_FLOAT,          v_x,                    &v_x)
+LOG_ADD(LOG_FLOAT,          v_y,                    &v_y)
+LOG_ADD(LOG_FLOAT,          v_z,                    &v_z)
+LOG_ADD(LOG_FLOAT,          w_x,                    &w_x)
+LOG_ADD(LOG_FLOAT,          w_y,                    &w_y)
+LOG_ADD(LOG_FLOAT,          w_z,                    &w_z)
+LOG_ADD(LOG_FLOAT,          o_x_des,                &o_x_des)
+LOG_ADD(LOG_FLOAT,          o_y_des,                &o_y_des)
+LOG_ADD(LOG_FLOAT,          o_z_des,                &o_z_des)
+LOG_ADD(LOG_FLOAT,          tau_x,                  &tau_x)
+LOG_ADD(LOG_FLOAT,          tau_y,                  &tau_y)
+LOG_ADD(LOG_FLOAT,          tau_z,                  &tau_z)
+LOG_ADD(LOG_FLOAT,          f_z,                    &f_z)
+LOG_ADD(LOG_UINT16,         m_1,                    &m_1)
+LOG_ADD(LOG_UINT16,         m_2,                    &m_2)
+LOG_ADD(LOG_UINT16,         m_3,                    &m_3)
+LOG_ADD(LOG_UINT16,         m_4,                    &m_4)
+LOG_ADD(LOG_FLOAT,          n_x,                    &n_x)
+LOG_ADD(LOG_FLOAT,          n_y,                    &n_y)
+LOG_ADD(LOG_FLOAT,          r,                      &r)
+LOG_ADD(LOG_FLOAT,          a_z,                    &a_z)
+LOG_GROUP_STOP(ae483log);
+PARAM_GROUP_START(ae483par)
+PARAM_ADD(PARAM_UINT8,     use_observer,            &use_observer)
+PARAM_ADD(PARAM_UINT8,     reset_observer,          &reset_observer)
+PARAM_GROUP_STOP(ae483par)
